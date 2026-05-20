472cf3c5f0598b458cda75b721e9bb6c1dad0361
Hdm <amwuje@126.com>
2026-05-20 10:33:03 +0800

    Change the multiplication sign to "×"  U+00d7

--- Calcpad.Core/Output/OutputWriter.cs
+++ Calcpad.Core/Output/OutputWriter.cs
@@ -228,7 +228,7 @@ namespace Calcpad.Core
         {
             Calculator.NegChar => "-",
             '-' => " − ",
-            '*' => "·",
+            '*' => "×",
             '/' => "/",
             '÷' => "/",
             '<' => " < ",
